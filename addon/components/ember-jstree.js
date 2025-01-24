dcd64bf373cb2c7985c2bb224953e66562c3df7e
Victor Elci <victor.elci@aboveproperty.com>
2025-01-24 12:50:01 -0500

    fix jquery deprecation

--- addon/components/ember-jstree.js
+++ addon/components/ember-jstree.js
@@ -102,7 +102,7 @@ export default Component.extend(InboundActions, EmberJstreeActions, {
    * @method _setupJsTree
    */
   _setupJsTree() {
-    return this.$().jstree(this._buildConfig());
+    return $(this.element).jstree(this._buildConfig());
   },
 
   /**
